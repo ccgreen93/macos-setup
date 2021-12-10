b00904240acb852a05b9b49dda9a2ee25350233a
Chris Green <ccgreen93@gmail.com>
2021-12-10 16:53:06 +1100

    latest changes

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install work related packages
+brew install ansible
 brew install --cask authy
 brew install awscli
 brew install --cask bitwarden
 brew install --cask brave-browser
+brew install --cask docker
 brew install --cask google-cloud-sdk
+brew install graphwiz #(for visualising terraform)
 #brew install --cask iterm2
 brew install --cask microsoft-remote-desktop
 brew install --cask microsoft-teams
@@ -22,11 +25,49 @@ brew install terraform-docs
 brew install tflint
 brew install warrensbox/tap/tfswitch
 brew install --cask visual-studio-code
+brew install watch
+brew install yamllint
 
-# Powerlevel10K
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-# add p10k settings
+## Terminal
+# replace below with fish? - https://fishshell.com/
+
+# install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+# install Powerlevel10K theme - will prompt to customise on next run
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+# echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+
+# auto suggestions - https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# add 'zsh-autosuggestions' to plugins in ~/.zshrc
+# plugins=( 
+#     # other plugins...
+#     zsh-autosuggestions
+# )
+
+# https://github.com/zsh-users/zsh-history-substring-search#usage:
+# "If you want to use zsh-syntax-highlighting along with this script, 
+# then make sure that you load it before you load this script"
+# source zsh-syntax-highlighting.zsh
+# source zsh-history-substring-search.zsh
+
+# syntax highlighting
+brew install zsh-syntax-highlighting
+echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>~/.zshrc
+
+# zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+# add 'zsh-history-substring-search' to plugins in ~/.zshrc
+# plugins=( 
+#     # other plugins...
+#     zsh-history-substring-search
+# )
+
+# also kinda cool, needs iterm2: https://github.com/zsh-users/zsh-apple-touchbar
+
+
+# flat icon theme colours: https://github.com/QuentinWatt/dark-flat-iterm-colors/blob/master/flat-colors.itermcolors
+curl https://raw.githubusercontent.com/QuentinWatt/dark-flat-iterm-colors/master/flat-colors.itermcolors -o ~/Downloads/flat-colors.itermcolors
 
 # install additional packages
 brew install --cask spotify
@@ -48,6 +89,11 @@ echo '{
 "$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; /* Shift + Ctrl + Home */
 "$^\UF72B" = "moveToEndOfDocumentAndModifySelection:"; /* Shift + Ctrl + End */
 }' > $HOME/Library/KeyBindings/DefaultKeyBinding.dict
+
+
+
+# https://github.com/antonbabenko/pre-commit-terraform
+
 
 
 # change hostname
